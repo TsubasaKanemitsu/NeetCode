c2fe69604cc054786aec075d058e3f64a8a74943
TsubasaKanemitsu <kanemitsu@TsubasaKanemitsunoMacBook-Air.local>
2024-07-16 05:41:45 -0700

    fix: array and hashing no.3

--- arrays-and-hashing/two_sum.py
+++ arrays-and-hashing/two_sum.py
@@ -11,16 +11,13 @@ class Solution:
     #                 return [i, j]
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashMap = {}
-        for i in range(len(nums)):
-            idx = hashMap.get(target - nums[i], -1)
-            if idx == -1:
-                hashMap[nums[i]] = i
-                continue
-
-            if i > idx:
-                return list([idx, i])
-            return list([i, idx])
+        for i, n in enumerate(nums):
             
+            diff = target - nums[i]
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[nums[i]] = i
+                        
             
 
 nums = list(map(int, input().split()))
